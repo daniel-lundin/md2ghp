9861ca98f92f5cd606069ca879e21edc2cddb7cf
Daniel Lundin <d-dog@d-dog.se>
2017-10-14 22:02:43 +0200

    Write to temp dir

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const tmp = require('tmp');
+const fs = require('fs');
+const path = require('path');
 const md2html = require('../src');
 
-console.log(md2html());
+const html = md2html();
+const css = fs.readFileSync('./assets/no-class.min.css');
+
+const tempDir = tmp.dirSync();
+const indexPath = path.join(tempDir.name, 'index.html');
+const cssPath = path.join(tempDir.name, 'no-class.min.css');
+fs.writeFileSync(indexPath, html);
+fs.writeFileSync(cssPath, css);
+
+
+console.log('indexPath', indexPath);
